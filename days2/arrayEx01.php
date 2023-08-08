9e5c62d0427aa369a73fd9fe4809257dcac46999
zack8361 <zack6353@naver.com>
2023-08-08 22:45:07 +0900

    phpExample
    phpExample

---
+++ days2/arrayEx01.php
@@ -0,0 +1,21 @@
+<?php 
+
+
+
+$age = array(
+  "아빠" => "40",
+  "엄마" => "38",
+  "나" => "12",
+);
+
+echo "<h1> 
+
+<body>
+</body>
+</h1>";
+// 배열 iteration
+foreach ($age as $key => $value) {
+  echo "<h2> $key</h2>";
+}
+
+?>
---
+++ days2/arrayEx02.php
@@ -0,0 +1,14 @@
+<?php
+
+// 2차원 배열(다차원 배열)
+
+$cars = array(
+  array("산타페", 10),
+  array("산타페", 10),
+  array("산타페", 110),
+  array("산타페", "110"),
+  array("하이", 10), array("찡그", "hi")
+
+);
+
+print_r($cars);
