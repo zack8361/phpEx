d1d69e3b9f46da09f85467b99a4cf232b0190d16
zack8361 <zack6353@naver.com>
2023-08-19 18:27:50 +0900

    회사 프로젝트 추가
    123

---
+++ project/model/objectMap.php
@@ -0,0 +1,23 @@
+<?php
+
+include "../model/db.php";
+class ObjectMap
+{
+    private $conn;
+
+    public function __construct($db)
+    {
+        $this->conn = $db;
+    }
+
+    // objectMapList 뽑기
+    public function objectMapList()
+    {
+        // 수정 1.
+        $sql = "SELECT * FROM tobjectmap";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->setFetchMode(PDO::FETCH_ASSOC);
+        $stmt->execute();
+        return $stmt->fetchAll();
+    }
+}
---
+++ project/view/main.php
@@ -0,0 +1,83 @@
+<?php
+include "../view/common/header.html";
+include "../model/db.php";
+include "../model/memer.php";
+include "../model/objectMap.php";
+// 세션 처리.
+session_start();
+$userId = isset($_SESSION['userId']) ? $_SESSION['userId'] : '';
+
+if ($userId == '') {
+    die("
+    <script> 
+    alert('접근 권한이 없습니다. 로그인 해주세요.');
+    self.location.href = './login.php';
+    </script>");
+}
+
+// model 객체 생성.
+$obj = new ObjectMap($db);
+$objMapList = $obj->objectMapList();
+
+$objJson = json_encode($objMapList);
+echo "<script> console.log($objJson);</script>"
+?>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <style>
+        .card-container {
+            text-align: center;
+        }
+
+        .image-container {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            margin: 20px;
+            height: 150px;
+            /* 조절할 이미지 높이 */
+        }
+
+        .card-image {
+            max-width: 100%;
+            max-height: 100%;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="album py-5 bg-body-tertiary">
+        <div class="container">
+            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+                <?php foreach ($objMapList as $value) { ?>
+                    <div class="col">
+                        <div class="card shadow-sm card-container" data-sObjectType="">
+                            <div class="image-container">
+                                <img src="<?= $value['sObjectImage']; ?>" class="card-image">
+                            </div>
+                            <div class="card-body">
+                                <p class="card-text"><?= $value['sObjectName']; ?></p>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <div class="btn-group" style="margin: auto;">
+                                        <button type="button" class="btn btn-sm btn-outline-secondary btn-view" id="btn-view" data-idx="<?= $value['sObjectType']; ?>">View</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                <?php } ?>
+            </div>
+        </div>
+    </div>
+    <script>
+        $('.btn-view').on("click", function() {
+            console.log("하이")
+            const sObjectType = $(this).data('idx');
+            self.location.href = `./subMain.php?objectType=${sObjectType}`;
+        })
+    </script>
+</body>
+<?php include "../view/common/footer.html"; ?>
